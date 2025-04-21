2134918bf373a9045aff626331a32d016a296d7b
Yukti-04 <reman.arora0411@gmail.com>
2025-04-21 22:43:59 +0530

    card game

--- memoryGame/script.mjs
+++ memoryGame/script.mjs
@@ -16,7 +16,7 @@ export function createGame(div) {
 const game = createGame(document.getElementById('game'));
 
 loadAssets(game,[
-    { name: "back", url: "./Assets/cardBack.png" },
+    { name: "back", url: "./Assets/cardback.png" },
     { name: "front", url: "./Assets/cardSprite.png" },
   ], () => {
     console.log('starting game');
